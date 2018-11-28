642190068df810e32cc02144ff05fe3f60c046fc
Juan Huttemann <juanfhuttemann@gmail.com>
2018-11-28 14:44:21 -0300

    moved calc to its own component

--- pages/calc.js
+++ pages/calc.js
@@ -1,27 +1,12 @@
 import Header from '../components/Header'
 import NavBar from '../components/NavBar'
+import Calc from '../components/Calc'
+
 export default ()=>(
     <div>
         <Header/>
         <NavBar/>
         <h1>Página de la Calculadora</h1>
-        <div className="z-depth-2 light-blue lighten-2 white-text center-align resultados">Area de numeros</div>
-        <div style={{textAlign: "center"}}>
-            <a className="waves-effect waves-light btn">0</a>
-            <a className="waves-effect waves-light btn">1</a>
-            <a className="waves-effect waves-light btn">2</a>
-        </div>
-        <style jsx>
-        {
-            `
-            .resultados {
-                width:300px;
-                margin:0 auto;
-                font-size: 32px;
-                margin-bottom: 10px;
-            }
-            `
-        }
-        </style>
+        <Calc/>
     </div>
 )
