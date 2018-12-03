93cfb579d71aa9c9c437454e558236b8055c5709
Juan Huttemann <juanfhuttemann@gmail.com>
2018-12-03 09:50:53 -0300

    calc component

--- components/Calc.js
+++ components/Calc.js
@@ -1,79 +1,83 @@
 import { Component } from 'react';
 
 class Calc extends Component {
-    state = {
-        res: "0",
-    }
+  state = {
+    res: "0",
+  }
 
-    onDigit = (digit)=>{
-        this.setState({ 
-            res: digit 
-        })
-    }
+  onDigit = (digit) => {
+    const expresion = this.state.res + digit
+    this.setState({
+      res: expresion
+    })
+  }
 
-    render() {
-        return (
-            <div>
-                <div className="z-depth-2 light-blue lighten-2 white-text center-align resultados">
-                    {this.state.res || "0"}
-                </div>
+  render() {
+    return (
+      <div>
+        <div className="z-depth-2 light-blue lighten-2 white-text center-align resultados">
+          {this.state.res || "0"}
+        </div>
 
-                <div className="calc-container blue lighten-3 hoverable z-depth-2">
-                    {
-                        ["0","1","2","3","4","5","6","7","8","9","."].map(digit=>{
-                                return (
-                                    <a 
-                                    className="waves-effect waves-light btn" 
-                                    onClick={() => this.onDigit(digit)}>
-                                        {digit}
-                                    </a>
-                                )
-                            }
-                        )
-                    }
-                </div>
-                <div className="operadores center-align">
-                    {
-                        ["+","-","/","*",""].map(digit=>{
-                                return (
-                                    <a
-                                        className="waves-effect waves-light btn deep-orange darken-1"
-                                        onClick={() => this.onDigit(digit)}>
-                                        {digit === "" ? "C" : digit}
-                                    </a>
-                                )
-                            }
-                        )
-                    }
-                </div>
-                <style jsx>
-                    {`
-                        .resultados {
-                            width:300px;
-                            margin:0 auto;
-                            font-size: 32px;
-                            margin-bottom: 10px;
-                        }
+        <div className="calc-container blue lighten-3 hoverable z-depth-2">
+          {
+            ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."].map(digit => {
+              return (
+                <a
+                  className="waves-effect waves-light btn"
+                  onClick={() => this.onDigit(digit)}>
+                  {digit}
+                </a>
+              )
+            }
+            )
+          }
+          <div className="operadores center-align">
+            {
+              ["+", "-", "/", "*", ""].map(digit => {
+                return (
+                  <a
+                    className="waves-effect waves-light btn deep-orange darken-1"
+                    onClick={() => this.onDigit(digit)}>
+                    {digit === "" ? "C" : digit}
+                  </a>
+                )
+              }
+              )
+            }
+          </div>
+        </div>
 
-                        .calc-container {
-                            width: 300px;
-                            margin: 0 auto;
-                            text-align:center;
-                            padding: 12px;
-                        }
+        <style jsx>
+          {`
+            .resultados {
+              width:300px;
+              margin:0 auto;
+              font-size: 32px;
+              margin-bottom: 10px;
+            }
 
-                        .operadores {
-                            border: 1px solid #ccc;
-                            padding: 10px;
-                            margin-top: 20px;
+            .calc-container {
+              width: 300px;
+              margin: 0 auto;
+              text-align:center;
+              padding: 12px;
+            }
 
+            .calc-container a {
+              margin: 5px;
+            }
 
-                        }
-                    `}
-                </style>
-            </div>
-        );
-    }
+            .operadores {
+              border: 1px solid #ccc;
+              padding: 10px;
+              margin-top: 20px;
+            }
+        `}
+        </style>
+      </div>
+    );
+  }
 }
 
 export default Calc;
